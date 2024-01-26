01ff47e3197abb6cb6657e963e7a2e08a251cf04
Koe <bueshalua22@gmail.com>
2024-01-26 15:31:12 +0600

    fr

--- Tsis3/Class.py
+++ Tsis3/Class.py
@@ -16,3 +16,21 @@ p1 = Person("John", 36)
 
 print(p1.name)
 print(p1.age)
+
+class Student(Person):
+  a= "Alua"
+
+print(Student.a)
+
+class Person:
+  def __init__(self, fname):
+    self.firstname = fname
+
+  def printname(self):
+    print(self.firstname)
+
+class Student(Person):
+  pass
+
+x = Student("Mike")
+x.printname()
